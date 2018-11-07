7940b42da3e0de6f52055a3cd25f512a03927859
avilchezgr <32094694+avilchezgr@users.noreply.github.com>
2018-11-07 10:40:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+class Layout extends Component {
+	constructor(){
+		super();
+		this.state = ({showSideBar:false});
+	}
+	
+	handleToggleSideBar =() =>{
+		this.setState({showSideBar:this.state.showSideBar ? false : true });
+	}
+	render(){
+		const {showSideBar} = this.state;
+		return(
+		
+		<div className="layout">
+			{showSideBar && (	
+				<SideBar 
+				onHide={this.handleToggleSideBar}
+				showSideBar={showSideBar}
+				/>		
+			)}
+				
+				
+			<Content 
+				clase ={showSideBar ? "content notFullContent" : "content fullContent"}
+				onShow={this.handleToggleSideBar}
+				isSideBarVisible={this.state.showSideBar}/>
+		</div>
+		);
+	}
+	
+}
+class Content extends Component {
+	render(){	
+		return (
+			<div className={this.props.clase}>
+				<h2>Content</h2>
+				{!this.props.isSideBarVisible &&
+				<BotonHideSideBar handleToggleSideBar={this.props.onShow}>Show</BotonHideSideBar>
+				}
+			</div>
+		);
+	}
+
+}
+class SideBar extends Component {
+	
+	render(){
+		
+		return (
+		<div className="sideBar">
+				<h2>SideBar</h2>
+		<BotonHideSideBar handleToggleSideBar={this.props.onHide}>Hide</BotonHideSideBar>
+				
+			</div>
+		);
+	}
+	
+}
+
+ const BotonHideSideBar = (props) => {
+	 return (
+		<button type="button" onClick={props.handleToggleSideBar}>{props.children}</button>
+	 );
+ }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <Layout/>
     );
   }
 }
